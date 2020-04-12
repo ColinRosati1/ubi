5c0cfad14109fa916c80a31fc8f47e5733ff6c81
ColinRosati1 <colin.rosati@finiata.com>
2020-04-12 12:35:16 +0200

    Updates

---
+++ precache-manifest.42a126ae8a43fba0366cf1c928bf391a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4768ace95c98c44f9dc43e8bb8f21325",
+    "url": "/ubi/index.html"
+  },
+  {
+    "revision": "d37e0d6c5a3bd442d3a5",
+    "url": "/ubi/static/css/main.71f6b879.chunk.css"
+  },
+  {
+    "revision": "db2c76085021b0e8eea0",
+    "url": "/ubi/static/js/2.363a30ba.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ubi/static/js/2.363a30ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37e0d6c5a3bd442d3a5",
+    "url": "/ubi/static/js/main.b6ea6284.chunk.js"
+  },
+  {
+    "revision": "de516eea2c9088a1edec",
+    "url": "/ubi/static/js/runtime-main.29102722.js"
+  }
+]);
