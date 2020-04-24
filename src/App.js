181c923056dc15774b33b85da2e4f7b7134b3424
ColinRosati1 <colin.rosati@finiata.com>
2020-04-24 16:25:02 +0200

    Cleanup IndexWrapper

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import { DirectoryContainer } from './components/container/Directory';
 import IndexBody from './components/dedicated/Index/IndexBody';
-
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/directory" component={DirectoryContainer}>
-        </Route>
+        <Route exact path="/directory" component={DirectoryContainer}></Route>
         <Route path="/" component={IndexBody}></Route>
       </Switch>
-      </Router>
+    </Router>
   );
 }
 
--- src/components/dedicated/Directory/IndexWrapper/index.tsx
+++ src/components/dedicated/Directory/IndexWrapper/index.tsx
@@ -6,18 +6,22 @@ import styles from './IndexWrapper.module.scss';
 import IndexCard from 'components/core/IndexCard';
 import Loader from 'components/core/Loader';
 import { Ubi } from 'types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const IndexWrapper: FC = () => {
   const ubiFiltered: Ubi[] = useSelector(state => state.ubiFilter);
   const ubiList: Ubi[] = useSelector(state => state.ubiList);
   const _filtered = ubiFiltered.length === 0 ? ubiList : ubiFiltered;
-  const _body = !!ubiList.length ? (
-    Object.values(_filtered).map((item, index) => <IndexCard key={`${index}`} {...item} />)
-  ) : (
-    <Loader />
-  );
+  const filteredItems = Object.values(_filtered).map((item, index) => (
+    <IndexCard key={`${index}`} {...item} />
+  ));
+  const _body = !!!ubiList.length ? filteredItems : <Loader />;
 
-  return <div className={styles.wrapper}>{_body}</div>;
+  return (
+    <div className={styles.wrapper}>
+      {!!ubiList.length ? <div>{filteredItems}</div> : <Loader />}
+    </div>
+  );
 };
 
 export default IndexWrapper;
