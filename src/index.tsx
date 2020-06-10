70ff66b596affa67a44a52553994ca18d1ba07ad
ColinRosati1 <colin.rosati@finiata.com>
2020-06-10 12:35:41 +0200

    Add helmet-react for SEO meta tags title description

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
 import './index.css';
 import App from './App';
@@ -9,6 +10,15 @@ import store from 'store/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Universal Basic Income Catalogue</title>
+      <link rel="canonical" href="https://colinrosati1.github.io/ubi/" />
+      <meta
+        name="description"
+        content="An easy to ready catalogue of Universal Basic Income from past to present."
+      />
+    </Helmet>
     <Provider store={store}>
       <App />
     </Provider>
